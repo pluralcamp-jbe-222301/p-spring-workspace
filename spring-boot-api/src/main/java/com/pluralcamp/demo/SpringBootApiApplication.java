e370184ca6864055ad54fe9f6ddfe376165782bb
Oriol Boix Anfosso <orboan@ilg.cat>
2023-02-06 19:05:45 +0000

    datasource fixed

--- spring-boot-api/src/main/java/com/pluralcamp/demo/SpringBootApiApplication.java
+++ spring-boot-api/src/main/java/com/pluralcamp/demo/SpringBootApiApplication.java
@@ -9,6 +9,7 @@ import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 
