b00030218394baecb45f2a7ed905f5f66309a39c
Oriol Boix Anfosso <orboan@pluralcamp.com>
2023-02-07 16:56:07 +0000

    updated

--- spring-boot-web/src/main/java/com/pluralcamp/demo/SpringBootWebApplication.java
+++ spring-boot-web/src/main/java/com/pluralcamp/demo/SpringBootWebApplication.java
@@ -1,7 +1,5 @@
 package com.pluralcamp.demo;
 
-import java.util.Collections;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -11,12 +9,12 @@ public class SpringBootWebApplication {
 	public final static String API_URL = "http://localhost:8080";
 
 	public static void main(String[] args) {
-		SpringApplication app = 
-				new SpringApplication(SpringBootWebApplication.class);
-		app.setDefaultProperties(
-				Collections.singletonMap("server.port", "8081"));
-		app.run(args);	
-//		SpringApplication.run(SpringBootWebApplication.class, args);
+//		SpringApplication app = 
+//				new SpringApplication(SpringBootWebApplication.class);
+//		app.setDefaultProperties(
+//				Collections.singletonMap("server.port", "8081"));
+//		app.run(args);	
+		SpringApplication.run(SpringBootWebApplication.class, args);
 	}
 
 }
